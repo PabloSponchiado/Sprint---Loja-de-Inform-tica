f5af6c69f631edcd4a7df9d03934cf1cced208b4
PABLO ISAAC SPONCHIADO SILVA <pablo.sponchiado@portalsesisp.org.br>
2026-08-19 16:53:40 -0300

    feat(server): setup express server and middleware

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+import { router } from "./router.js";
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+server.use(router);
+export { server };
