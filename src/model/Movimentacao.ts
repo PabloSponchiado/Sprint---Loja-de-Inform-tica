02110d7cc8e96e57ee927655ec8e0ca7b34ca46d
PABLO ISAAC SPONCHIADO SILVA <pablo.sponchiado@portalsesisp.org.br>
2026-08-19 16:52:46 -0300

    refactor(model): update Movimentacao model imports and DTO usage

--- src/model/Movimentacao.ts
+++ src/model/Movimentacao.ts
@@ -1,24 +1,7 @@
 import { DatabaseModel } from "./DatabaseModel.js";
+import type MovimentacaoDTO from "../interface/MovimentacaoDTO.js";
 
 const database = new DatabaseModel().pool;
-
-export interface MovimentacaoDTO {
-    id_movimentacao: number;
-    id_produto: number;
-    id_movimentacao_origem: number | null;
-    tipo: string;
-    motivo: string;
-    quantidade: number;
-    preco_unitario_praticado: number | null;
-    valor_total: number | null;
-    observacao: string;
-    data_movimentacao: Date;
-    produto?: {
-        id_produto: number;
-        codigo: string;
-        nome: string;
-    } | null;
-}
 
 class Movimentacao {
     private id_movimentacao: number = 0;
