c99ede81f0f1f38edc7d4ea373c479b18eff7805
PABLO ISAAC SPONCHIADO SILVA <pablo.sponchiado@portalsesisp.org.br>
2026-08-19 16:53:24 -0300

    feat(interface): add ProdutoDTO

---
+++ src/interface/ProdutoDTO.ts
@@ -0,0 +1,16 @@
+export default interface ProdutoDTO {
+    id_produto: number;
+    id_categoria: number;
+    codigo: string;
+    nome: string;
+    descricao?: string | null;
+    preco_unitario: number;
+    quantidade_disponivel: number;
+    quantidade_minima: number;
+    ativo: boolean;
+    data_cadastro: Date;
+    categoria?: {
+        id_categoria: number;
+        nome?: string | null;
+    } | null;
+}
